b956b05d65831e4dcf5664718336ad62ecbf4532
Vix Sak <sakv@shernet.sheridancollege.ca>
2026-01-09 11:50:28 -0500

    Intoduced public variables into the first script, started the rollover script. (started week 2 pre work)

---
+++ Assets/Scripts/Rollover.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Rollover : MonoBehaviour
+{
+    public bool mouseIsOverMe = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the mouse position (in pixels) and convert it to world space (in meters)
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        //get the distance between transform.position and the mouse position
+        float distance = Vector2.Distance(transform.position, mousePos);
+        //if that distanc is small (<1) set mouseIsOverMe to true
+        if (distance < 1)
+        {
+            mouseIsOverMe = true;
+        }
+        else
+        {
+            //otherwise set mouseIsOverM to false
+            mouseIsOverMe= false;
+        }
+    }
+}
