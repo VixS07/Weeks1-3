bb7028cbca3264e8c5051a67a947b03bf3cec27a
Vix Sak <sakv@shernet.sheridancollege.ca>
2026-01-16 11:50:31 -0500

    Started rotation

---
+++ Assets/Scripts/PointMe.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PointMe : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the mouse positon
+
+        //work out the direction betweem our position and the mouse position
+        //set our transform.up direction to equal that
+    }
+}
