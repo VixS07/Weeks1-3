c64d6b2aca9d40e202db8b9d75b2918941513ec6
MOTH_SANCTUARY\vixsa <vix.sak@gmail.com>
2026-01-26 14:32:54 -0500

    Finished the stars. Fixed the errors in the code and made them move back ssslightly as the bat is flying

--- Assets/Mechanical Draing/Scripts/Twinkle.cs
+++ Assets/Mechanical Draing/Scripts/Twinkle.cs
@@ -6,7 +6,8 @@ public class Twinkle : MonoBehaviour
 {
     public AnimationCurve curve;
     public float t;
-    public bool hi;
+    Vector2 bottomLeft;
+    Vector2 topRight;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,7 +29,7 @@ public class Twinkle : MonoBehaviour
         topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
 
         //reset the time every 5 seconds to allow for a loop
-        if (t > 5) {
+        if (t > 2) {
             t = 0;
             //change the position to a random area within the screen
             //im doing unit circle because im not sure how to combine random with world screen position
@@ -38,29 +39,42 @@ public class Twinkle : MonoBehaviour
             //I was going to do this but I think we did something like this in class week 3 and I dont want to risk it lol
 
             //Random.Range
+
         }
+
+        
 
         //check to see if star has moved off screen, if it has bring it to the edge of the screen it went over.
         //x axis
         //left side
-        if (newPos.x < 0)
+        if (newPos.x < bottomLeft.x)
         {
             newPos.x = bottomLeft.x;
         }
         //right side
-        else if (newPos.x > Screen.width)
+        else if (newPos.x > topRight.x)
         {
             newPos.x = topRight.x;
         }
         //y axis
         //top
-        else if (newPos.y < 0) {
-            newPos.y = bottomLeft.x
+        else if (newPos.y < bottomLeft.y) 
+        {
+            newPos.y = bottomLeft.y;
         }
-        else if (newPos.y > Screen.height)
+        else if (newPos.y > topRight.y)
         {
             newPos.y = topRight.y;
         }
+
+        //make the stars move back slightly to keep the illusion of the bat flying
+        if (t <= 2)
+        {
+            newPos.x -= 1 * Time.deltaTime;
+        }
+
+        //update pos
+        transform.position = newPos;
 
         //change the scale of the star using the animation curve based on time.
         //got from the Pulse script
